337e8a46b74a0ce3f6a9ad9bbdd1c7334407a667
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2024-11-06 07:10:12 +0530

    Time: 0 ms (100%), Space: 41.5 MB (47.92%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,18 +13,47 @@
  *     }
  * }
  */
+ //recursive approach
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer>res=new ArrayList<>();
+// postorder(root,res);
+// return res;
+//     }
+//     private void postorder(TreeNode node, List<Integer>res){
+//         if(node==null){
+//             return;
+//         }
+//         postorder(node.left,res);
+//         postorder(node.right,res);
+//         res.add(node.val);
+//     }
+// }
+
+
+// using 2 stack
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer>res=new ArrayList<>();
-postorder(root,res);
-return res;
-    }
-    private void postorder(TreeNode node, List<Integer>res){
-        if(node==null){
-            return;
+        List<Integer>postorder=new ArrayList<>();
+        if(root==null){
+            return postorder;
         }
-        postorder(node.left,res);
-        postorder(node.right,res);
-        res.add(node.val);
+        Stack<TreeNode>st1=new Stack<>();
+        Stack<TreeNode>st2=new Stack<>();
+st1.push(root);
+while(!st1.empty()){
+    root=st1.pop();
+    st2.push(root);
+    if(root.left!=null){
+        st1.push(root.left);
+    }
+    if(root.right!=null){
+        st1.push(root.right);
+    }
+}
+while(!st2.empty()){
+    postorder.add(st2.pop().val);
+}
+return postorder;
     }
 }

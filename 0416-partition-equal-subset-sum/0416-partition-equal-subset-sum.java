c90d29624bf39000252be280db4247041113ea2e
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2024-12-26 14:19:13 +0530

    Time: 42 ms (57.43%), Space: 45.2 MB (75.81%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,52 @@
 class Solution {
-    public boolean subsetSumUtil(int ind, int target, int[] nums,int [][] dp){
-        if(target==0)
-        return true;
-        if(ind==0)
-        return nums[0]==target;
-        if(dp[ind][target]!=-1)
-        return dp[ind][target]==1;
-        boolean notTaken=subsetSumUtil(ind-1,target,nums,dp);
-        boolean taken=false;
-        if(nums[ind]<=target)
-        taken=subsetSumUtil(ind-1,target-nums[ind],nums,dp);
-        dp[ind][target]=(notTaken || taken)? 1:0;
-        return notTaken || taken;
-    }
+    // public boolean subsetSumUtil(int ind, int target, int[] nums,int [][] dp){
+    //     if(target==0)
+    //     return true;
+    //     if(ind==0)
+    //     return nums[0]==target;
+    //     if(dp[ind][target]!=-1)
+    //     return dp[ind][target]==1;
+    //     boolean notTaken=subsetSumUtil(ind-1,target,nums,dp);
+    //     boolean taken=false;
+    //     if(nums[ind]<=target)
+    //     taken=subsetSumUtil(ind-1,target-nums[ind],nums,dp);
+    //     dp[ind][target]=(notTaken || taken)? 1:0;
+    //     return notTaken || taken;
+    // }
     public boolean canPartition(int[] nums) {
 int n=nums.length;
+
+
+
+// tabulation approach 
 int totsum=0;
-for(int num:nums){
-    totsum+=num;
+for(int i=0;i<n;i++){
+    totsum+=nums[i];
 
 }
-if(totsum%2!=0)
+if(totsum%2==1)
 return false;
-int target=totsum/2;
-int [][] dp=new int[n][target+1];
-for(int[] row:dp)
-Arrays.fill(row,-1);
-return subsetSumUtil(n-1,target,nums,dp);
-        
+
+    int k=totsum/2;
+    boolean dp[][]=new boolean [n][k+1];
+    for(int i=0;i<n;i++){
+        dp[i][0]=true;
     }
+    if(nums[0]<=k){
+        dp[0][nums[0]]=true;
+    }
+    for(int ind=1;ind<n;ind++){
+        for(int target=1;target<=k;target++){
+            boolean notTaken = dp[ind - 1][target];
+            boolean taken=false;
+            if(nums[ind]<=target){
+                taken=dp[ind-1][target-nums[ind]];
+
+            }
+        
+            dp[ind][target]=notTaken||taken;
+        }
+    }
+    return dp[n-1][k];
 }
+    }

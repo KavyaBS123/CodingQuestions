68a24d51d6c113ab075f16ab781333fc2242f973
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2024-12-27 18:53:27 +0530

    Time: 109 ms (8.37%), Space: 45 MB (27.31%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,38 @@
 class Solution {
-    static int minimumelementsutil(int [] coins,int ind, int amount, int[][] dp){
-        if(ind==0){
-            if(amount%coins[0]==0)
-            return amount/coins[0];
+     public int coinChange(int[] coins, int amount) {
+   int n=coins.length;
+        int [][] dp=new int [n][amount+1];
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)
+            dp[0][i]=i/coins[0];
             else
-            return (int) Math.pow(10,9);
+            dp[0][i]=(int) Math.pow(10,9);
         }
-        if(dp[ind][amount]!=-1)
-        return dp[ind][amount];
-        int notTaken=0+minimumelementsutil(coins,ind-1,amount,dp);
+        // if(ind==0){
+        //     if(amount%coins[0]==0)
+        //     return amount/coins[0];
+        //     else
+        //     return (int) Math.pow(10,9);
+        // }
+        // if(dp[ind][amount]!=-1)
+        // return dp[ind][amount];
+        for(int ind=1;ind<n;ind++){
+            for(int target=0;target<=amount;target++){
+                
+            
+        
+        int notTaken=0+dp[ind-1][target];
         int taken=(int) Math.pow(10,9);
-        if(coins[ind]<=amount)
-        taken=1+minimumelementsutil(coins,ind, amount-coins[ind],dp);
-        return dp[ind][amount]=Math.min(notTaken,taken);
+        if(coins[ind]<=target)
+        taken=1+dp[ind][target-coins[ind]];
+     dp[ind][target]=Math.min(notTaken,taken);
 
     }
-    public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
-        int [][] dp=new int [n][amount+1];
-        for(int row[]:dp)
-        Arrays.fill(row,-1);
-        int ans=minimumelementsutil(coins,n-1,amount,dp);
-        if(ans>=(int) Math.pow(10,9))
+   
+        
+        }
+        int ans=dp[n-1][amount];
+        if(ans>=(int)Math.pow(10,9))
         return -1;
         return ans;
     }

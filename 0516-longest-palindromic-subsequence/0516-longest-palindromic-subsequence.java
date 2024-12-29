475c657158f7789d55a86a2f67bcdb7a1a326f8a
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2024-12-29 21:56:41 +0530

    Time: 53 ms (47.18%), Space: 45.1 MB (95.56%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -2,24 +2,21 @@ class Solution {
     static int lcs(String s1, String s2){
         int n=s1.length();
         int m=s2.length();
-        int dp[][] =new int[n+1][m+1];
-        for(int rows[]:dp)
-        Arrays.fill(rows,-1);
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-        for(int i=0;i<=m;i++){
-            dp[0][i]=0;
-        }
+       int[] prev=new int[m+1];
+       int[]cur=new int[m+1];
+        
+        
+        
         for(int ind1=1;ind1<=n;ind1++){
             for(int ind2=1;ind2<=m;ind2++){
                 if(s1.charAt(ind1-1)==s2.charAt(ind2-1))
-                dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+                cur[ind2]=1+prev[ind2-1];
                 else
-                dp[ind1][ind2]=Math.max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                cur[ind2]=Math.max(prev[ind2],cur[ind2-1]);
             }
+            prev=cur.clone();
         }
-        return dp[n][m];
+        return prev[m];
     }
     public int longestPalindromeSubseq(String s) {
         String reversed= new StringBuilder(s).reverse().toString();

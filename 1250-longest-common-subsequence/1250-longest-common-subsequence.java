e86504ab11e6a9a70e768ad7bfaf4d169be0a1d3
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2024-12-29 20:54:50 +0530

    Time: 18 ms (91.1%), Space: 45.3 MB (91.5%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -4,26 +4,21 @@ class Solution {
         // Tabulation tech:
         int n=text1.length();
         int m=text2.length();
-        int [][] dp=new int[n+1][m+1];
-        for(int rows[]:dp)
-            Arrays.fill(rows,-1);
-            for(int i=0;i<=n;i++){
-                dp[i][0]=0;
+      int prev[]=new int[m+1];
+      int cur[]=new int[m+1];
 
-            }
-            for(int i=0;i<=m;i++){
-                dp[0][i]=0;
-            }
+          
             for(int ind1=1;ind1<=n;ind1++){
                 for(int ind2=1;ind2<=m;ind2++){
                 if(text1.charAt(ind1-1)==text2.charAt(ind2-1))
-                dp[ind1][ind2]=1+dp[ind1-1][ind2-1];
+                cur[ind2]=1+prev[ind2-1];
                 else
-                dp[ind1][ind2]=Math.max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                cur[ind2]=Math.max(prev[ind2],cur[ind2-1]);
                 }
+            prev =(int[])(cur.clone());
+            
             }
-            return dp[n][m];
-           
+            return prev[m];
         }
         
     }

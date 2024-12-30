0279732559a6c0823a8bfc6b0f223a90102cdbed
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2024-12-30 18:53:14 +0530

    Time: 5 ms (64.3%), Space: 45.1 MB (49.97%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,24 +1,23 @@
 class Solution {
-   static int editdistanceutil(String word1, String word2,int i, int j, int[][] dp){
-   
-        if(i<0)
-        return j+1;
-        if(j<0)
-        return i+1;
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        if(word1.charAt(i)==word2.charAt(j))
-        return dp[i][j]=editdistanceutil(word1,word2,i-1,j-1,dp);
-        else
-        return dp[i][j]=1+Math.min(editdistanceutil(word1,word2,i-1,j-1,dp),
-        Math.min(editdistanceutil(word1,word2,i-1,j,dp),editdistanceutil(word1,word2,i,j-1,dp)));
-    }
-     public int minDistance(String word1, String word2) {
-        int n=word1.length();
+    public int minDistance(String word1, String word2) {
+          int n=word1.length();
         int m=word2.length();
-        int [][] dp=new int[n][m];
-        for(int row[]:dp)
-        Arrays.fill(row,-1);
-        return editdistanceutil(word1,word2,n-1,m-1,dp);
+         int [][] dp=new int[n+1][m+1];
+         for(int i=0;i<=n;i++){
+            dp[i][0]=i;
+         }
+         for(int j=0;j<=m;j++){
+            dp[0][j]=j;
+         }
+         for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                dp[i][j]=dp[i-1][j-1];
+            } else{
+                dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+            }
+         }
+    }
+    return dp[n][m];
 }
 }

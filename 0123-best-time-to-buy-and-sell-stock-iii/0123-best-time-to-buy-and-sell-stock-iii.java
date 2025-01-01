e23a299d29fe0c3263939054d94ed29184021771
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-01-01 10:09:17 +0530

    Time: 63 ms (40.95%), Space: 64.3 MB (30.19%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,33 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][3];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                Arrays.fill(dp[i][j], -1);
-            }
-        }
-        return getans(prices, n, 0, 0, 2, dp);
-    }
+        int[][][] dp = new int[n+1][2][3];
+       for(int ind=n-1;ind>=0;ind--){
+        for(int buy=0;buy<=1;buy++){
+            for(int cap=1;cap<=2;cap++){
 
-    static int getans(int[] arr, int n, int ind, int buy, int cap, int[][][] dp) {
-        if (ind == n || cap == 0) {
-            return 0;
-        }
-        if (dp[ind][buy][cap] != -1) {
-            return dp[ind][buy][cap];
-        }
-
-        int profit = 0; // Initialize `profit` to ensure it has a value in all cases.
-
-        if (buy == 0) {
-            profit = Math.max(0 + getans(arr, n, ind + 1, 0, cap, dp),
-                    -arr[ind] + getans(arr, n, ind + 1, 1, cap, dp));
+            
+               if (buy == 0) {
+            dp[ind][buy][cap] = Math.max(0 + dp[ind+1][0][cap],
+                    -prices[ind] +dp[ind+1][1][cap]);
         } else if (buy == 1) {
-            profit = Math.max(0 + getans(arr, n, ind + 1, 1, cap, dp),
-                    arr[ind] + getans(arr, n, ind + 1, 0, cap - 1, dp));
+            dp[ind][buy][cap] = Math.max(0 + dp[ind+1][1][cap],
+                    prices[ind]+dp[ind+1][0][cap-1]);
         }
-
-        return dp[ind][buy][cap] = profit;
-    }
+       }
+       }
+       }
+       return dp[0][0][2];
+}
 }

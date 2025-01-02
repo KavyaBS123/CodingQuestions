f78f87f83f9814af57ee2f4f4f32af1ecbc54cc2
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-01-02 09:18:57 +0530

    Time: 17 ms (48.55%), Space: 55 MB (81.4%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,40 +1,33 @@
 class Solution {
-     static int getans(int[] Arr, int ind, int buy, int n, int fee, int[][] dp) {
-        // Base case
-        if (ind == n) {
-            return 0;
-        }
+     
 
-        // If the result is already calculated, return it
-        if (dp[ind][buy] != -1) {
-            return dp[ind][buy];
-        }
-
-        int profit = 0;
-
-        if (buy == 0) { // We can buy the stock
-            profit = Math.max(0 + getans(Arr, ind + 1, 0, n, fee, dp), -Arr[ind] + getans(Arr, ind + 1, 1, n, fee, dp));
+    public int maxProfit(int[] prices, int fee) {
+        int n=prices.length;
+      
+       if(n==0){
+        return 0;
+       }
+       int dp[][]=new int[n+1][2];
+       for(int ind=n-1;ind>=0;ind--){
+        for(int buy=0;buy<=1;buy++){
+            int profit=0;
+             if (buy == 0) { // We can buy the stock
+            profit = Math.max(0 + dp[ind+1][0],
+             -prices[ind] + dp[ind+1][1]);
         }
 
         if (buy == 1) { // We can sell the stock
-            profit = Math.max(0 + getans(Arr, ind + 1, 1, n, fee, dp), Arr[ind] - fee + getans(Arr, ind + 1, 0, n, fee, dp));
+            profit = Math.max(0 + dp[ind+1][1],
+             prices[ind] - fee + dp[ind+1][0]);
         }
 
         // Store the result in dp and return it
         dp[ind][buy] = profit;
-        return profit;
+    
     }
-
-    public int maxProfit(int[] prices, int fee) {
-        int n=prices.length;
-       int dp[][]=new int[n][2];
-       for(int row[]:dp){
-        Arrays.fill(row,-1);
-       } 
-       if(n==0){
-        return 0;
+        
        }
-       int ans=getans(prices,0,0,n,fee,dp);
-       return ans;
+       return dp[0][0];
+      
     }
 }

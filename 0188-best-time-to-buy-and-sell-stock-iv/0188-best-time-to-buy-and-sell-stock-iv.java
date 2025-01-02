a56bfd0f638cf86bac3be66beaed5725a0328ca7
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-01-02 08:32:58 +0530

    Time: 2 ms (81.97%), Space: 44.9 MB (33.08%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,24 +1,28 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n=prices.length;
+        int[][] ahead=new int[2][k+1];
+        int [] [] cur=new int[2][k+1];
         int [][][] dp=new int[n+1][2][k+1];
        for(int ind=n-1;ind>=0;ind--){
         for(int buy=0;buy<=1;buy++){
             for(int cap=1;cap<=k;cap++){
                 if(buy==0){
-            dp[ind][buy][cap]=Math.max(0+dp[ind+1][0][cap],
-            -prices[ind]+dp[ind+1][1][cap]);
+            cur[buy][cap]=Math.max(0+ahead[0][cap],
+            -prices[ind]+ahead[1][cap]);
         } else {
-dp[ind][buy][cap]=Math.max(0+dp[ind+1][1][cap],
-prices[ind]+dp[ind+1][0][cap-1]);
+cur[buy][cap]=Math.max(0+ahead[1][cap],
+prices[ind]+ahead[0][cap-1]);
         }
+            }
      
+    }
+    for(int i=0;i<2;i++){
+        System.arraycopy(cur[i],0,ahead[i],0,k+1);
     } 
             
         }
+        return ahead[0][k];
        }
- return dp[0][0][k];    
-    }
-
-      
+ 
 }

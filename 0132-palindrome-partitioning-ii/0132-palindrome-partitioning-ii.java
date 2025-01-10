ba03d55c101bcf4592b1c1af90bf4155e46deb8b
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-01-10 07:05:15 +0530

    Time: 2495 ms (24.24%), Space: 41.8 MB (66.99%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,24 +1,21 @@
 class Solution {
     public int minCut(String s) {
         int n=s.length();
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        return f(0,n,s,dp)-1;
-        
-    }
-    static int f(int i, int n, String s, int[] dp){
-        if(i==n)
-        return 0;
-        if(dp[i]!=-1) return dp[i];
-        int mincost=Integer.MAX_VALUE;
-        for(int j=i;j<n;j++){
-            if(ispalindrome(i,j,s)){
-                int cost=1+f(j+1,n,s,dp);
-                mincost=Math.min(mincost,cost);
+        int[] dp=new int[n+1];
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int minCost=Integer.MAX_VALUE;
+            for(int j=i;j<n;j++){
+                if(ispalindrome(i,j,s)){
+                    int cost=1+dp[j+1];
+                    minCost=Math.min(minCost,cost);
+                }
             }
+            dp[i]=minCost;
         }
-        return dp[i]=mincost;
+        return dp[0]-1;
     }
+       
     static boolean ispalindrome(int i, int j, String s)
 {
     while(i<j){

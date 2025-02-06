18fed882098f990c12396d4fa57e76b292068bfa
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-02-06 20:40:15 +0530

    Time: 0 ms (100%), Space: 41 MB (5.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -67,14 +67,29 @@
 // }
 //tabulation approach:
 
-class Solution {
+// class Solution {
+// public int climbStairs(int n) {
+//     int dp[]=new int[n+1];
+//     dp[0]=1;
+//     dp[1]=1;
+//     for(int i=2;i<n+1;i++){
+//         dp[i]=dp[i-1]+dp[i-2];
+//     }
+//     return dp[n];
+// }
+// }
+
+//optimal sol:
+ class Solution {
 public int climbStairs(int n) {
-    int dp[]=new int[n+1];
-    dp[0]=1;
-    dp[1]=1;
-    for(int i=2;i<n+1;i++){
-        dp[i]=dp[i-1]+dp[i-2];
+    int a=1;
+    int b=1;
+    int c;
+    for(int i=0;i<n-1;i++){
+        c=a+b;
+        a=b;
+        b=c;
     }
-    return dp[n];
+    return b;
 }
-}
+ }

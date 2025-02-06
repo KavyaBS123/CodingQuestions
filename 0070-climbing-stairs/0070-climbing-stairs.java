d248c699dbb6ab9e1dcdffc9419cdaa308f12fbf
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-02-06 20:34:04 +0530

    Time: 0 ms (100%), Space: 40.2 MB (90.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,17 @@
-class Solution {
-    public int climbStairs(int n) {
-        // optimal
-        int a=1;
-        int b=1;
-        int c;
-        for(int i=0;i<n-1;i++){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return b;
-    }
-}
+// class Solution {
+//     public int climbStairs(int n) {
+//         // optimal
+//         int a=1;
+//         int b=1;
+//         int c;
+//         for(int i=0;i<n-1;i++){
+//             c=a+b;
+//             a=b;
+//             b=c;
+//         }
+//         return b;
+//     }
+// }
 // tabulation
 // class Solution {
 //     public int climbStairs(int n) {
@@ -43,3 +43,25 @@ class Solution {
 // memo[n]=climbstairs(n-1,memo)+climbstairs(n-2,memo);
 // return memo[n];
 //     }
+
+
+
+// memoization approach
+class Solution {
+     public int climbStairs(int n) {
+        int memo[]= new int[n+1];
+        Arrays.fill(memo,-1);
+        return climbstaris(n-1,memo)+climbstaris(n-2,memo);
+     }
+private int climbstaris(int n, int memo[]){
+    if(n<0) return 0;
+    if(n==0 || n==1 ){
+
+    memo[n]=1;
+    return memo[n];
+}
+if(memo[n]!=-1) return memo[n];
+memo[n]=climbstaris(n-1,memo)+climbstaris(n-2,memo);
+return memo[n];
+}
+}

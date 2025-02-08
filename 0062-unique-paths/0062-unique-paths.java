15594dcc7f31693542523250e966dde98cb25f1f
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-02-08 20:37:05 +0530

    Time: 0 ms (100%), Space: 40.9 MB (11%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,46 +1,70 @@
-// class Solution {
-//     static int countwaysutil(int i, int j, int [][] dp){
-//         if(i==0 && j==0)
-//         return 1;
-//         if(i<0 ||j<0)
-//         return 0;
-//         if(dp[i][j]!=-1)
-//         return dp[i][j];
-//         int up=countwaysutil(i-1,j,dp);
-//         int left=countwaysutil(i,j-1,dp);
-//         return dp[i][j]=up+left;
-//     }
-//     public int uniquePaths(int m, int n) {
-//         int dp[][]=new int[m][n];
-//         for(int[] row:dp)
-//         Arrays.fill(row,-1);
-//         return countwaysutil(m-1,n-1,dp);
+// // class Solution {
+// //     static int countwaysutil(int i, int j, int [][] dp){
+// //         if(i==0 && j==0)
+// //         return 1;
+// //         if(i<0 ||j<0)
+// //         return 0;
+// //         if(dp[i][j]!=-1)
+// //         return dp[i][j];
+// //         int up=countwaysutil(i-1,j,dp);
+// //         int left=countwaysutil(i,j-1,dp);
+// //         return dp[i][j]=up+left;
+// //     }
+// //     public int uniquePaths(int m, int n) {
+// //         int dp[][]=new int[m][n];
+// //         for(int[] row:dp)
+// //         Arrays.fill(row,-1);
+// //         return countwaysutil(m-1,n-1,dp);
         
-//     }
+// //     }
+// // }
+// class Solution {
+//      static int countwaysutil(int m, int n, int [][] dp){
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 && j==0){
+//                     dp[i][j]=1;
+//                     continue;
+//                 }
+//                 int up=0;
+//                 int left=0;
+//                 if(i>0)
+//                 up=dp[i-1][j];
+//                 if(j>0)
+//                 left=dp[i][j-1];
+//                 dp[i][j]=up+left;
+//             }
+//         }
+//         return dp[m-1][n-1];
+//      }
+//      public int uniquePaths(int m, int n) {
+//         int dp[][]=new int[m][n];
+//         for(int [] row:dp)
+//         Arrays.fill(row,-1);
+//         return countwaysutil(m,n,dp);
+//      }
 // }
+
 class Solution {
-     static int countwaysutil(int m, int n, int [][] dp){
+ public int uniquePaths(int m, int n){
+        int prev[]=new int[n];
         for(int i=0;i<m;i++){
+            int temp[]=new int[n];
             for(int j=0;j<n;j++){
                 if(i==0 && j==0){
-                    dp[i][j]=1;
+                    temp[j]=1;
                     continue;
                 }
                 int up=0;
                 int left=0;
                 if(i>0)
-                up=dp[i-1][j];
+                up=prev[j];
                 if(j>0)
-                left=dp[i][j-1];
-                dp[i][j]=up+left;
+                left=temp[j-1];
+                temp[j]=up+left;
             }
+            prev=temp;
         }
-        return dp[m-1][n-1];
-     }
-     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-        for(int [] row:dp)
-        Arrays.fill(row,-1);
-        return countwaysutil(m,n,dp);
+        return prev[n-1];
      }
 }

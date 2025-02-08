2c59ead973f204f22989afba61b7c8a6250e008e
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-02-08 20:19:41 +0530

    Time: 0 ms (100%), Space: 40.4 MB (59.82%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int countwaysutil(int i, int j, int [][] dp){
+        if(i==0 && j==0)
+        return 1;
+        if(i<0 ||j<0)
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int up=countwaysutil(i-1,j,dp);
+        int left=countwaysutil(i,j-1,dp);
+        return dp[i][j]=up+left;
+    }
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        for(int[] row:dp)
+        Arrays.fill(row,-1);
+        return countwaysutil(m-1,n-1,dp);
+        
+    }
+}

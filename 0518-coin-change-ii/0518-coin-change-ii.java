2f7f8c65584a8281364f6f277906eb9c3354fb36
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-02-15 09:59:49 +0530

    Time: 10 ms (50.95%), Space: 49.1 MB (44.23%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -15,27 +15,52 @@
 //     }
 // }
 // MEMOIZATION APPROACH:
-class Solution {
+// class Solution {
+//     public int change(int amount, int[] coins) {
+//         int n=coins.length;
+//         int dp[][]=new int[n][amount+1];
+//         for(int row[]:dp)
+//         Arrays.fill(row,-1);
+//         return countwaystomakechangeutil(coins,n-1,amount,dp);
+//     }
+//     static int countwaystomakechangeutil(int[] coins, int ind, int amount,int[][]dp){
+//         if(ind==0){
+//             if(amount%coins[0]==0)
+//             return 1;
+//             else
+//             return 0;
+//         }
+//         if(dp[ind][amount]!=-1)
+//         return dp[ind][amount];
+//         int nottaken=countwaystomakechangeutil(coins,ind-1,amount,dp);
+//         int taken=0;
+//         if(coins[ind]<=amount)
+//      taken=countwaystomakechangeutil(coins,ind,amount-coins[ind],dp);
+//      return dp[ind][amount]=nottaken+taken;
+//     }
+// }
+//TC:O(N*T)
+//SC:O(N*T)+O(N)
+
+
+//TABULATION APPROACH:
+ class Solution {
     public int change(int amount, int[] coins) {
-        int n=coins.length;
+        int n =coins.length;
         int dp[][]=new int[n][amount+1];
-        for(int row[]:dp)
-        Arrays.fill(row,-1);
-        return countwaystomakechangeutil(coins,n-1,amount,dp);
-    }
-    static int countwaystomakechangeutil(int[] coins, int ind, int amount,int[][]dp){
-        if(ind==0){
-            if(amount%coins[0]==0)
-            return 1;
-            else
-            return 0;
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0)
+            dp[0][i]=1;
         }
-        if(dp[ind][amount]!=-1)
-        return dp[ind][amount];
-        int nottaken=countwaystomakechangeutil(coins,ind-1,amount,dp);
-        int taken=0;
-        if(coins[ind]<=amount)
-     taken=countwaystomakechangeutil(coins,ind,amount-coins[ind],dp);
-     return dp[ind][amount]=nottaken+taken;
+        for(int ind=1;ind<n;ind++){
+            for(int target=0;target<=amount;target++){
+                int nottaken=dp[ind-1][target];
+                int taken=0;
+                if(coins[ind]<=target)
+                taken=dp[ind][target-coins[ind]];
+                dp[ind][target]=nottaken+taken;
+            }
+        }
+        return dp[n-1][amount];
     }
-}
+ }

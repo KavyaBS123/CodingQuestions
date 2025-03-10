866d23f8ccc040a008cb7fd4120b3a5932d2e7db
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-03-10 23:05:47 +0530

    Time: 1 ms (47.4%), Space: 44.8 MB (92.1%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,23 +1,28 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-      int inicolor=image[sr][sc];
-      int [][] ans=image;
-      int delrow[]={-1,0,+1,0};
-      int delcol[]={0,+1,0,-1};
-      dfs(sr,sc,ans,image,color,delrow,delcol,inicolor);
-      return ans;
+  
+int inicolor=image[sr][sc];
+int[][]ans=image;
+int delrow[]={-1,0,+1,0};
+int delcol[]={0,+1,0,-1};
+dfs(image,ans,sr,sc,inicolor,color,delrow,delcol);
+return ans;
     }
-    private void dfs(int row, int col,int [][] ans,int [][] image,int color,int delrow[],int delcol[],int inicolor){
-        ans[row][col]=color;
-        int n=image.length;
-        int m=image[0].length;
-        for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==inicolor && ans[nrow][ncol]!=color){
-                dfs(nrow,ncol,ans,image,color,delrow,delcol,inicolor);
-            }
 
+
+
+  private void dfs(int [][] image,int ans[][], int sr, int sc , int inicolor, int color,int delrow[],int delcol[]){
+    ans[sr][sc]=color;
+    int n=image.length;
+    int m=image[0].length;
+    for(int i=0;i<4;i++){
+        int nrow=sr+delrow[i];
+        int ncol=sc+delcol[i];
+        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&
+        image[nrow][ncol]==inicolor && ans[nrow][ncol]!=color){
+            dfs(image,ans,nrow,ncol,inicolor,color,delrow,delcol);
         }
+        
     }
-}
+  }
+ }

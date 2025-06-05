5a653adda454a6bc12a4986c345f37ac228e10a5
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-06-05 21:36:34 +0530

    Time: 2 ms (27.8%), Space: 57.3 MB (77.59%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,3 +1,21 @@
+// To rotate right by k:
+
+// Reverse the entire array.
+//  Example:
+
+// [1, 2, 3, 4, 5, 6, 7] → [7, 6, 5, 4, 3, 2, 1]
+// Reverse the first k elements.
+
+
+// [7, 6, 5, 4, 3, 2, 1] → [5, 6, 7, 4, 3, 2, 1]
+// Reverse the remaining n - k elements.
+
+
+// [5, 6, 7, 4, 3, 2, 1] → [5, 6, 7, 1, 2, 3, 4]
+
+
+
+
 class Solution {
     public void rotate(int[] nums, int k) {
     int l=0,r=nums.length-1;

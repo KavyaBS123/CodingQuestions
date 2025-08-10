5541a124745f5347375ad5a44a97dd625cfec1a8
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-08-10 14:37:22 +0530

    Time: 3 ms (29.74%), Space: 43.5 MB (5.37%) - LeetHub

--- 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
+++ 1574-maximum-product-of-two-elements-in-an-array/1574-maximum-product-of-two-elements-in-an-array.java
@@ -10,17 +10,26 @@ class Solution {
 //         return max;
 //     }
 // }
-int max1=0,max2=0;
-for(int num:nums){
-    if(num>max1){
-        max2=max1;
-        max1=num;
-    }
-    else if(num>max2){
-        max2=num;
-    }
+// int max1=0,max2=0;
+// for(int num:nums){
+//     if(num>max1){
+//         max2=max1;
+//         max1=num;
+//     }
+//     else if(num>max2){
+//         max2=num;
+//     }
 
-}
+// }
+// return (max1-1)*(max2-1);
+// }
+// }
+
+PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+for(int num:nums)
+pq.add(num);
+int max1=pq.poll();
+int max2=pq.poll();
 return (max1-1)*(max2-1);
-}
+    }
 }

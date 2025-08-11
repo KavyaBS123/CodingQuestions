7030a702d19f27424059228ee16b94e8a6e0252c
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2025-08-11 06:48:32 +0530

    Time: 0 ms (100%), Space: 41.9 MB (82.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,43 @@
 class Solution {
     public void sortColors(int[] nums) {
-   int low=0;
-   int high=nums.length-1;
-   int mid=0;
-   while(mid<=high){
-    if(nums[mid]==0){
-        int temp=nums[low];
-        nums[low]=nums[mid];
-        nums[mid]=temp;
-        mid++;
-        low++;
+
+        //Dutch National flag algo:
+
+        int low=0,mid=0,high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums,mid,high);
+                high--;
+            }
+        }
     }
-    else if(nums[mid]==2){
-        int temp=nums[high];
-        nums[high]=nums[mid];
-        nums[mid]=temp;
-        high--;
+        private void swap(int [] nums , int i , int j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
     }
-    else if(nums[mid]==1){
-        mid++;
-    }
-   }
-    }
-}
+//         int count0=0,count1=0,count2=0;
+//         for(int num:nums){
+//             if(num==0) count0++;
+//            else if(num==1) count1++;
+//             else count2++;
+
+//         }
+//         int index=0;
+//         while(count0-->0) nums[index++]=0;
+//         while(count1-->0) nums[index++]=1;
+//         while(count2-->0) nums[index++]=2;
+//     }
+// }
+
+
+

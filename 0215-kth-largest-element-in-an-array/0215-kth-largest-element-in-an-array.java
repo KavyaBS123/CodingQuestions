9e140aafc37aa9065ce163f7bf8d76d4a754c6a2
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2026-02-05 08:03:13 +0530

    Time: 69 ms (30.69%), Space: 68.7 MB (54.84%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,14 +1,34 @@
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         PriorityQueue<Integer>minheap=new PriorityQueue<>();
+//         for(int num:nums){
+//             minheap.offer(num);
+//             if(minheap.size()>k){
+//                 minheap.poll();
+//             }
+
+//         }
+//         return minheap.peek();
+        
+//         }
+// }
+
+
+// BRUTE FORCE:
+// Arrays.sort(nums);
+// return nums[nums.length-k];
+
+
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer>minheap=new PriorityQueue<>();
-        for(int num:nums){
-            minheap.offer(num);
-            if(minheap.size()>k){
-                minheap.poll();
-            }
+PriorityQueue<Integer>minheap=new PriorityQueue<>();
 
-        }
-        return minheap.peek();
-        
-        }
+for (int num:nums){
+    minheap.offer(num);
+    if(minheap.size()>k){
+        minheap.poll();
+    }
+}
+return minheap.peek();
+    }
 }

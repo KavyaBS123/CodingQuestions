27d724a8cc61d4d415424ccf04b80062f0fdab91
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2026-02-05 07:34:21 +0530

    Time: 3 ms (19.52%), Space: 47.7 MB (85.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,35 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//       int n=nums.length;
+   
+//     int lastnonzeroindex=0;
+//     for(int i=0;i<n;i++){
+//         if(nums[i]!=0){
+//             if(i!=lastnonzeroindex){
+//                 int temp=nums[i];
+//                 nums[i]=nums[lastnonzeroindex];
+//                 nums[lastnonzeroindex]=temp;
+//             }
+//             lastnonzeroindex++;
+//         }
+//     }
+//     }
+// }
+
+
 class Solution {
     public void moveZeroes(int[] nums) {
-      int n=nums.length;
-   
-    int lastnonzeroindex=0;
-    for(int i=0;i<n;i++){
-        if(nums[i]!=0){
-            if(i!=lastnonzeroindex){
-                int temp=nums[i];
-                nums[i]=nums[lastnonzeroindex];
-                nums[lastnonzeroindex]=temp;
+        int n=nums.length;
+        int lastnonzeroindex=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                if(i!=lastnonzeroindex){
+                    int temp=nums[i];
+                    nums[i]=nums[lastnonzeroindex];
+                    nums[lastnonzeroindex]=temp;
+                }
+                lastnonzeroindex++;
             }
-            lastnonzeroindex++;
         }
-    }
     }
 }

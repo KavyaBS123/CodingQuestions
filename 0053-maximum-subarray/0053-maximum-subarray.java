b712cb60f7bb7de2957a62593b8a7f65c6271a7c
Kavya B S <148982177+KavyaBS123@users.noreply.github.com>
2026-02-05 07:22:47 +0530

    Time: 1 ms (99.58%), Space: 77.5 MB (17.95%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,25 +1,40 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        int maxsum=nums[0];
+        int maxend=nums[0];
+        int n=nums.length;
+        for(int start =1;start<n;start++){
+            maxend=Math.max(nums[start],maxend+nums[start]);
+            maxsum=Math.max(maxsum,maxend);
+        }
+        return maxsum;
+    }
+}
+           
+
+// int maxsofar=nums[0];
+// int maxendinghere=nums[0];
+// for(int i=1;i<nums.length;i++){
+//    maxendinghere=Math.max(nums[i],maxendinghere+nums[i]);
+//    maxsofar=Math.max(maxsofar,maxendinghere);
+// }
+// return maxsofar;
+//     }
+// }
+
+
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
 //         int maxsum=Integer.MIN_VALUE;
 //         int n=nums.length;
-//         for(int start =0;start<n;start++){
+//         for(int i=0;i<n;i++){
 //             int currentsum=0;
-//             for(int end=start;end<n;end++){
-//                 currentsum+=nums[end];
-//                 maxsum=Math.max(maxsum, currentsum);
+//             for(int j=i;j<n;j++){
+// currentsum+=nums[j];
+// maxsum=Math.max(currentsum, maxsum);
 //             }
 //         }
 //         return maxsum;
 //     }
 // }
-
-
-int maxsofar=nums[0];
-int maxendinghere=nums[0];
-for(int i=1;i<nums.length;i++){
-   maxendinghere=Math.max(nums[i],maxendinghere+nums[i]);
-   maxsofar=Math.max(maxsofar,maxendinghere);
-}
-return maxsofar;
-    }
-}
